819cc3e96b139725d547b1d294cb21745998809e
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2025-11-12 14:18:35 +0200

    solo-dovira-fixx

--- src/js/hero.js
+++ src/js/hero.js
@@ -14,23 +14,5 @@ function changeSlide(n) {
   hero.style.backgroundImage = `url(${images[index]})`;
 }
 
-// універсальна функція створення кнопки зі спрайта
-function createArrow(direction, iconId, handler) {
-  const button = document.createElement('button');
-  button.className = `hero-arrow ${direction}`;
-  button.innerHTML = `
-    <svg class="arrow-icon" width="24" height="24" aria-hidden="true">
-      <use href="./img/icons.svg#${iconId}"></use>
-    </svg>
-  `;
-  button.addEventListener('click', handler);
-  return button;
-}
-
-const leftArrow = createArrow('left', 'icon-ileft', () => changeSlide(-1));
-const rightArrow = createArrow('right', 'icon-iright', () => changeSlide(1));
-
-hero.append(leftArrow, rightArrow);
-
 // автоперемикання
 setInterval(() => changeSlide(1), 7000);

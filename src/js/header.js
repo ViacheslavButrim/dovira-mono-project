27d07e2fc744fbdd04e25dd3c7068c4d6da05454
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2025-11-21 16:21:24 +0200

    solo-dovira-fix

--- src/js/header.js
+++
@@ -1,8 +0,0 @@
-const navLinks = document.querySelectorAll('.nav-link');
-
-navLinks.forEach(link => {
-  link.addEventListener('click', e => {
-    navLinks.forEach(l => l.classList.remove('current'));
-    e.currentTarget.classList.add('current');
-  });
-});
--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import './js/mobile-menu.js';
 import './css/styles.css';
-import './js/header.js'
-import './js/hero.js'
-import './js/brands.js'
-import './js/map.js'
+import './js/brands.js';
 

ffd7f753bad3ad57377c478b2ffb0f0b98a68565
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2025-11-28 18:16:55 +0200

    dovira-works-gradient-fix

--- src/js/works.js
+++ src/js/works.js
@@ -2,9 +2,23 @@ document.querySelectorAll(".compare").forEach(block => {
   const slider = block.querySelector(".slider");
   const after = block.querySelector(".after");
   const handle = block.querySelector(".glass-handle");
-  slider.addEventListener("input", e => {
-    const v = e.target.value;
-    after.style.clipPath = `inset(0 ${100 - v}% 0 0)`;
-    handle.style.left = v + "%";
+  
+  let lastValue = slider.value;
+  let ticking = false;
+
+  function updateSlider() {
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        after.style.clipPath = `inset(0 ${100 - lastValue}% 0 0)`; // Плавне зміщення
+        handle.style.left = `${lastValue}%`;  // Переміщуємо маркер ползунка
+        ticking = false;
+      });
+      ticking = true;
+    }
+  }
+
+  slider.addEventListener("input", (e) => {
+    lastValue = e.target.value;
+    updateSlider(); // Оновлюємо позицію зображення і маркера
   });
 });

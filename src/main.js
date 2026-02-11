8db48f21f61c1306ec6e4f8cd20dc57363463879
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2026-02-11 15:55:02 +0200

    fix-build

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './js/mobile-menu.js';
 import './css/styles.css';
-//import './js/brands.js';
 import './js/dovira-agent.js';
 import './js/works.js';
 document.addEventListener("DOMContentLoaded", () => {

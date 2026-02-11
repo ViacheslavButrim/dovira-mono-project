a5adcd9fe1c956e7552faf6b374fc86b7f67b1a2
Viacheslav Butrim <viacheslavbutrim@gmail.com>
2026-02-11 15:59:33 +0200

    fix-base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+     base: process.env.VERCEL ? '/' : '/dovira-mono-project/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
